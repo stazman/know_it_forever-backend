e559581135cd0de59f1e023955434ad4492638e3
stazman <cadistasio@gmail.com>
2023-01-16 13:46:29 -0600

    Corrects file system organization; updates dependencies

---
+++ know_it_forever_backend_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KnowItForeverBackendApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'know_it_forever_backend_api'
